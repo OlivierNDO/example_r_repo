f915fc78d720d5080a0188cc99125a501798ba90
OlivierNDO <Olivier_N@lynchburg.edu>
2020-02-11 17:51:36 -0600

    commit unit test of zscore_normalize

---
+++ test/test_zscore_normalize.R
@@ -0,0 +1,15 @@
+# Load Modules
+req_pkgs = c('dplyr', 'testthat')
+lapply(req_pkgs, require, character.only = TRUE)
+require(testthat)
+source("src/zscore_normalize.R")
+
+# Execute Unit Test
+test_that('Test zscore_normalize(c(10,50,100))',{
+  #' Unit test of zscore_normalize() function
+  result = c(10,50,100) %>%
+    zscore_normalize() %>%
+    median() %>%
+    round(2)
+  testthat::expect_equal(result, -0.07)
+})
